ac0670def377d348352a9e2a745b3c4b09362b22
jayone <imdff0803@gmail.com>
2020-05-24 19:49:12 +0900

    Update the front-end to make it more user-friendly

--- static/main.js
+++ static/main.js
@@ -6,12 +6,15 @@
   app.controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
     function ($scope, $log, $http, $timeout) {
 
+      $scope.submitButtonText = 'Submit'
+      $scope.loading = false
+      $scope.urlerror = false
+
       $scope.getResults = function () {
         $log.log("test")
 
         // get the URL from the input
         var userInput = $scope.url
-        $log.log(userInput)
 
         // fire the API request
         $http.post('/start', { 'url': userInput }).then(
@@ -20,6 +23,10 @@
             var jobID = results.data
             $log.log(jobID)
             getWordCount(jobID)
+            $scope.wordcounts = null
+            $scope.loading = true
+            $scope.submitButtonText = 'Loading...'
+            $scope.urlerror = false
           },
           function (error) {
             // error callback
@@ -32,13 +39,14 @@
 
         var poller = function () {
           // fire another request
-          $log.log('requesting ' + jobID)
           $http.get('/results/' + jobID).then(
-            function(resp) {
+            function (resp) {
               if (resp.status === 202) {
                 $log.log(resp.data, resp.status)
               } else if (resp.status === 200) {
                 $log.log(resp.data)
+                $scope.loading = false
+                $scope.submitButtonText = 'Submit'
                 $scope.wordcounts = resp.data
                 $timeout.cancel(timeout)
                 return false
@@ -46,6 +54,12 @@
               // continue to call the poller function every 2 seconds
               // until time timeout is cancelled
               timeout = $timeout(poller, 2000)
+            },
+            function (error) {
+              $log.log(error)
+              $scope.loading = false
+              $scope.submitButtonText = "Submit"
+              $scope.urlerror = true
             }
           )
         }
